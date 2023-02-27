69d7621318dead6c6f43a8d7db6e7180c1e69888
HunterM <zakaspro@gmail.com>
2023-02-27 22:19:53 +0300

    270223 v4

--- java/units/Archer.java
+++ java/units/Archer.java
@@ -17,9 +17,12 @@ public abstract class Archer extends Human {
     @Override
     public void step(ArrayList<Human> team1, ArrayList<Human> team2) {
         if (state.equals("Умер") || cartridges == 0) return;
+//        условие либо умер либо нет стрел покидает State
+
         Human victim = team2.get(findNearest(team2));
+//        Поиск ближайшего врага
             float damage = (victim.defense - attack) > 0 ? damageMin : (victim.defense - attack) < 0 ? damageMax : (damageMin + damageMax) / 2;
-        victim.getDamage(damage);
+            victim.getDamage(damage);
         for (Human human : team1) {
             if (human.getInfo().toString().split(":")[0].equals("Фермер") && human.state.equals("Stand")) {
                 human.state = "Занят";
